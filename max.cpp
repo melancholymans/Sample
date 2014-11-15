d623d0ed6ef75ef1404dc52bfcf26f244c14909d
joshua <kjkq26993@gaia.eonet.ne.jp>
2014-11-15 16:59:41 +0900

    constの使い方

--- max.cpp
+++ max.cpp
@@ -5,9 +5,19 @@
 int main()
 {
 	int i = 42;
+	int k = 64;
+	int const N = 100;
+	int* const bookmark = &i;	//int変数を指すポインタ変数bookmarkが定数となる
+
+
+	const int n = 25;
 	std::string str;
 
 	std::cout << ::max(7,1) << std::endl;
+	std::cout << *bookmark << std::endl;
+	//bookmark = &k;	コンパイラーエラー
+	i = 24;		//ポインタbookmarkは定数なので変更できないが,ポインタが指している変数は変更できる
+	std::cout << *bookmark << std::endl;
 
 	getchar();
 	return 1;
